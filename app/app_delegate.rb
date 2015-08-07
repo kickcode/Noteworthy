88e617506bc681c01b6f9315f7a569d2a8649983
Elliott Draper <el@kickcode.com>
2015-08-07 14:26:06 +0100

    working note adding and table view with in memory notes

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,43 @@ class AppDelegate
   def applicationDidFinishLaunching(notification)
     buildMenu
     buildWindow
+
+    @title_text = @layout.get(:title_text)
+    @content_text = @layout.get(:content_text)
+
+    @notes = []
+    @table_view = @layout.get(:table_view)
+    @table_view.delegate = self
+    @table_view.dataSource = self
+
+    @save_button = @layout.get(:save_button)
+    @save_button.target = self
+    @save_button.action = 'note_save:'
+  end
+
+  def note_save(sender)
+    title = @title_text.stringValue
+    content = @content_text.stringValue
+    return if title.nil? || title.empty? || content.nil? || content.empty?
+    @notes << {:title => title, :content => content}
+    @table_view.reloadData
+    @title_text.stringValue = ""
+    @content_text.stringValue = ""
+  end
+
+  def numberOfRowsInTableView(table_view)
+    @notes.length
+  end
+
+  def tableView(table_view, viewForTableColumn: column, row: row)
+    result = table_view.makeViewWithIdentifier(column.identifier, owner: self)
+    if result.nil?
+      result = NSTextField.alloc.initWithFrame([[0, 0], [column.width, 0]])
+      result.identifier = column.identifier
+      result.editable = false
+    end
+    result.stringValue = @notes[row][column.identifier.to_sym]
+    result
   end
 
   def buildWindow
@@ -11,5 +48,8 @@ class AppDelegate
       defer: false)
     @mainWindow.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
     @mainWindow.orderFrontRegardless
+
+    @layout = MainLayout.new
+    @mainWindow.contentView = @layout.view
   end
 end
---
+++ app/main_layout.rb
@@ -0,0 +1,78 @@
+class MainLayout < MotionKit::Layout
+  FORM_HEIGHT = 100
+  TITLE_HEIGHT = 30
+  CONTENT_HEIGHT = 70
+  BUTTON_WIDTH = 70
+  BUTTON_HEIGHT = 30
+
+  def layout
+    add NSView, :form do
+      constraints do
+        width.equals(:superview)
+        height.is(FORM_HEIGHT)
+
+        min_left.is 0
+        min_top.is 0
+      end
+
+      add NSTextField, :title_text do
+        placeholderString "Enter note title"
+
+        constraints do
+          width.equals(:superview)
+          height.is(TITLE_HEIGHT)
+
+          min_left.is 0
+          min_top.is 0
+        end
+      end
+
+      add NSTextField, :content_text do
+        placeholderString "Enter note content"
+
+        constraints do
+          width.equals(:superview)
+          height.is(CONTENT_HEIGHT)
+
+          min_left.is 0
+          min_top.is TITLE_HEIGHT
+        end
+      end
+
+      add NSButton, :save_button do
+        title "Save"
+
+        constraints do
+          width.equals(BUTTON_WIDTH)
+          height.is(BUTTON_HEIGHT)
+
+          min_right.is 0
+          min_bottom.is 0
+        end
+      end
+    end
+    add NSScrollView, :scroll_view do
+      has_vertical_scroller true
+      constraints do
+        width.equals(:superview)
+        height.equals(:superview).minus(FORM_HEIGHT)
+
+        min_top.is FORM_HEIGHT
+      end
+
+      table_view = add(NSTableView, :table_view) do
+        row_height 25
+
+        add_column "title" do
+          title "Title"
+          min_width 150
+        end
+
+        add_column "content" do
+          title "Content"
+        end
+      end
+      document_view table_view
+    end
+  end
+end
